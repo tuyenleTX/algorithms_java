e7c72c56a1b5972e26c5c9e900f39751b197a976
tuyenleTX <ttle@miu.edu>
2025-09-10 21:44:19 -0500

    searching string

---
+++ src/main/java/linkedinpractise/BasicStringParsingAndSearching.java
@@ -0,0 +1,20 @@
+package linkedinpractise;
+
+public class BasicStringParsingAndSearching {
+    public static void main(String[] args) {
+        String s = "HeLLo";
+        System.out.println(s.contains("LL"));
+
+        parsingString(s);
+    }
+    public static void parsingString(String s) {
+        System.out.println("Option 1:");
+        for (char c : s.toCharArray()) {
+            System.out.println(c);
+        }
+        System.out.println("Option 2:");
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
