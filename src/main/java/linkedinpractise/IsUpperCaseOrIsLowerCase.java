c7a0ccf8becf7353df1fcd194bd1216280c531d5
tuyenleTX <ttle@miu.edu>
2025-09-10 21:22:13 -0500

    Upper or Lower Case example

---
+++ src/main/java/linkedinpractise/IsUpperCaseOrIsLowerCase.java
@@ -0,0 +1,19 @@
+package linkedinpractise;
+
+public class IsUpperCaseOrIsLowerCase {
+    public static boolean isUpperCase(String s) {
+        return s.chars().allMatch(Character::isUpperCase);
+    }
+
+    public static boolean isLowerCase(String s) {
+        return s.chars().allMatch(Character::isLowerCase);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUpperCase("hello"));
+        System.out.println(isUpperCase("HELLO"));
+
+        System.out.println(isLowerCase("hello"));
+        System.out.println(isLowerCase("HELLO"));
+    }
+}
---
+++ src/main/java/linkedinpractise/VerifyPassword.java
@@ -0,0 +1,7 @@
+package linkedinpractise;
+
+public class VerifyPassword {
+    public static void main(String[] args) {
+
+    }
+}
