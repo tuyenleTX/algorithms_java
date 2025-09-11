29e9d7eafd8df04402808bdbcba3d5e7cf26bdd2
tuyenleTX <ttle@miu.edu>
2025-09-10 21:26:46 -0500

    verify password

--- src/main/java/linkedinpractise/VerifyPassword.java
+++ src/main/java/linkedinpractise/VerifyPassword.java
@@ -2,6 +2,14 @@ package linkedinpractise;
 
 public class VerifyPassword {
     public static void main(String[] args) {
-
+        System.out.println(verifyPassword("Hell0"));
+        System.out.println(verifyPassword("Hello"));
+        System.out.println(verifyPassword("hello"));
+        System.out.println(verifyPassword("HELLO"));
+    }
+    public static boolean verifyPassword(String s) {
+        return (s.chars().anyMatch(c -> Character.isDigit(c)) &&
+                s.chars().anyMatch(c -> Character.isLowerCase(c)) &&
+                s.chars().anyMatch(c -> Character.isUpperCase(c)));
     }
 }
