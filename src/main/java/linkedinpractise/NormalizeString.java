473d74a8a5a4d78592bfc670424a4aabfe1b60b1
tuyenleTX <ttle@miu.edu>
2025-09-10 21:46:28 -0500

    normalize string

---
+++ src/main/java/linkedinpractise/NormalizeString.java
@@ -0,0 +1,12 @@
+package linkedinpractise;
+
+import java.util.Locale;
+
+public class NormalizeString {
+    public static String normalizeString(String s) {
+        return s.toLowerCase().trim().replace(",", "");
+    }
+    public static void main(String[] args) {
+        System.out.println(normalizeString("      HELLO there, BUddy!      "));
+    }
+}
