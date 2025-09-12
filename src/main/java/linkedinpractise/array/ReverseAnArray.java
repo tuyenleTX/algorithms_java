88c5f6c776caacf290fc30b951dfafe466cfa4ed
tuyenleTX <ttle@miu.edu>
2025-09-11 22:47:30 -0500

    reverse array

--- src/main/java/linkedinpractise/array/ReverseAnArray.java
+++ src/main/java/linkedinpractise/array/ReverseAnArray.java
@@ -1,3 +1,5 @@
+package linkedinpractise.array;
+
 public class ReverseAnArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
--- src/main/java/linkedinpractise/array/ReverseArray2.java
+++ src/main/java/linkedinpractise/array/ReverseArray2.java
@@ -1,3 +1,5 @@
+package linkedinpractise.array;
+
 public class ReverseArray2 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
